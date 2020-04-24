59d01ddd5555d7a51932e369a94a8076a216f332
nerstak <33179821+nerstak@users.noreply.github.com>
2020-04-24 13:31:19 +0200

    Removed IGNORE

--- SQL-BDD/Insert.sql
+++ SQL-BDD/Insert.sql
@@ -1,4 +1,4 @@
-INSERT IGNORE INTO job(nom) VALUES
+INSERT INTO job(nom) VALUES
 ('Agriculteur'),
 ('Artiste'),
 ('Cadre'),
@@ -11,36 +11,36 @@ INSERT IGNORE INTO job(nom) VALUES
 ('Professeur'),
 ('Saisonnier');
 
-INSERT IGNORE INTO adresse(pays, region, ville, departement, code_postal, numero, rue) VALUES
+INSERT INTO adresse(pays, region, ville, departement, code_postal, numero, rue) VALUES
 ('France', 'Ile-de-France', 'Paris', 'Paris', '75000', '30', 'Champs-Elysee'),
 ('France', 'Ile-de-France', NULL, 'Essonne', NULL, NULL, NULL),
 ('France', 'Bretagne', 'Vannes', 'Morbihan', NULL, NULL, NULL),
 ('Outre-Mer', 'Guyane', 'Cayenne', 'Guyane', NULL, NULL, NULL);
 
-INSERT IGNORE INTO patient(nom, prenom, dob, categorie, moyen, email, password, id_adresse) VALUES
+INSERT INTO patient(nom, prenom, dob, categorie, moyen, email, password, id_adresse) VALUES
 ('Pascal', 'Dupont', '1945-04-17', 'adulte', 'Pages-Jaunes', 'pascal.dupont@gmail.com', '1234', 1),
 ('Roger', 'Dupond', '1985-04-17', 'adulte', 'Internet', 'rogerdu91@toutanota.com', '12345', 2),
 ('Anna', 'Belle', '2004-02-29', 'adolescente', 'Patient', 'annabelle@free.fr', 'mdp123', 3),
 ('Léa', 'Soutil', '1983-01-23', 'adulte', 'Bouche-a-Oreille', 'lea.soutil@sfr.fr', '#Ma1234', 4),
 ('Huges', 'Dupuit', '2012-06-30', 'enfant', 'Docteur', 'huges.dupuit@hotmail.fr', '1234', NULL);
 
-INSERT IGNORE INTO type_rdv(type_rdv, prix) VALUES
+INSERT INTO type_rdv(type_rdv, prix) VALUES
 ('familial', 40),
 ('conjugal', 30),
 ('normal', 15);
 
-INSERT IGNORE INTO rdv(id_type_rdv, date_rdv, paiement) VALUES
+INSERT INTO rdv(id_type_rdv, date_rdv, paiement) VALUES
 (1, '2020-04-17', 'Carte-Bleue'),
 (2, '2019-08-17', 'Espece'),
 (3, '2020-03-15', 'Cheque');
 
-INSERT IGNORE INTO historique_job(id_patient, id_job, date_debut, date_fin) VALUES
+INSERT INTO historique_job(id_patient, id_job, date_debut, date_fin) VALUES
 (1, 11, '2019-06-01', '2019-08-31'),
 (2, 10, '2000-09-05', NOW()),
 (3, 11, '2019-06-01', '2019-08-31'),
 (4, 3, '1999-03-18', NOW());
 
-INSERT IGNORE INTO consultation(id_rdv, id_patient, posture, gestuel, mots_clés, anxiete) VALUES
+INSERT INTO consultation(id_rdv, id_patient, posture, gestuel, mots_clés, anxiete) VALUES
 (1, 1, 'Ne se tient pas droit', NULL, 'pas droit', 5),
 (2, 2, 'Ne se tient pas droit', 'regarde dans le vide, semble detruit interieurement', 'las', 5),
 (3, 4, NULL, 'Bouge les mains sans cesse, ne fait que parler', 'surexitee', 7);
